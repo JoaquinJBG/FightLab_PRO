57c3632856fdaf96064f20e680ebc0ddb4daf118
JoaquinJBG <joaquinjesusbeltrangarcia@gmail.com>
2026-06-02 10:45:33 +0200

    style: overlay negro 0.1 sobre la nebulosa para oscurecer un poco el fondo

--- frontend/components/background-fx.tsx
+++ frontend/components/background-fx.tsx
@@ -22,6 +22,8 @@ export function BackgroundFx() {
       {/* Partículas + viñeta por encima */}
       <div className="dots" />
       <div className="vignette" />
+      {/* Capa oscura para bajar la intensidad del fondo */}
+      <div className="bg-overlay" />
     </div>
   );
 }
